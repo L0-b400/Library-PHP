66efe96beb6caed241d5677b1ec963d1081b54d5
L0-b400 <149195983+L0-b400@users.noreply.github.com>
2025-05-22 18:24:56 -0300

    init project
    init

---
+++ PHP/index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Bem vindo ao Sistema de Livrarias";
+echo "<br>";
+echo '<br> <a href="">Entrada</a>';
+echo '<br> <a href="">Cadastrar Empresas</a>';
+echo '<br> <a href="">Pesquisar Livros</a>';
+echo '<br> <a href="">Logout</a>';
+
+
+?>
